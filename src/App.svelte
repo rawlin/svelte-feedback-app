3f5e6998174da5fcdb82a9945e2b6fe051049227
Rawlin <rawlincrasto@gmail.com>
2022-07-10 22:34:57 +0530

    44:41 min

--- src/App.svelte
+++ src/App.svelte
@@ -1,66 +1,52 @@
 <script lang="ts">
-	let firstName = 'Rawlin'
-	let lastName = 'Crasto'
-	let color = 'blue'
-	let showText = false
-	$: name = firstName + ' ' + lastName
-	let users = [
-		{
-			id: '1',
-			name: 'John'
-		},
-		{
-			id: '2',
-			name: 'Sara'
-		},
-		{
-			id: '3',
-			name: 'Mike'
-		}
-	]
 
-	const toggle = () => {
-		color = color === 'blue' ? 'red' : 'blue'
-		showText = !showText
+import FeedbackForm from "./components/FeedbackForm.svelte";
+import FeedbackList from "./components/FeedbackList.svelte";
+import FeedbackStats from "./components/FeedbackStats.svelte"
+
+let feedback = [
+	{
+		id: 1,
+		rating: 10,
+		text: 'lorem ipsum adflak'
+	}, 
+	{
+		id: 2,
+		rating: 10,
+		text: 'lorem ipsum adflak'
+	},
+	{
+		id: 3,
+		rating: 10,
+		text: 'lorem ipsum adflak'
+	},
+	{
+		id: 4,
+		rating: 10,
+		text: 'lorem ipsum adflak'
 	}
+]
+
+$: count = feedback.length
+$: average = feedback.reduce((a, {rating}) => a + rating, 0) / feedback.length
+const deleteFeedback = (e) => {
+	const itemId = e.detail
+
+	feedback = feedback.filter((item) => item.id != itemId)
+	
+}
+
 </script>
 
-<main>
-	<h1 style="color: {color}">Hello {name}!</h1>
-	{#if showText}
-		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="container">
+	<FeedbackForm></FeedbackForm>
+	<FeedbackStats {count} {average}/>
+	<h1>{count}</h1>
+	<h1>{average}</h1>
+	<FeedbackList {feedback} on:delete-feedback=
+	{deleteFeedback} />
 
-	{:else}
-		<p>No Text</p>
-
-	{/if}
-	<button on:click={toggle}>Click</button>
-
-	{#each users as user (user.id)}
-
-	<h3>{user.id}: {user.name}</h3>
-		
-	{/each}
 </main>
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
